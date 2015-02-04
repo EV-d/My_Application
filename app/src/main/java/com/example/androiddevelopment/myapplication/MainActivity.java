411079a853e871f561f092642f99ce75687212d3
androiddevelopment <anwarsalim07@gmail.com>
2015-02-04 14:56:07 +0400

    add button

--- app/src/main/java/com/example/androiddevelopment/myapplication/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/myapplication/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.androiddevelopment.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
-
+Button an;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
